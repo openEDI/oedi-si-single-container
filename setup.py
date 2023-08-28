28e155e399ac4f5c7a9133ceddca305b15d728ce
Karthik <kbalasubramaniam@anl.gov>
2023-08-28 17:42:53 -0400

    Fix for windows pip issue

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(name='oedisi_single_container',
 	author = 'Karthikeyan Balasubramaniam',
 	author_email='kbalasubramaniam@anl.gov',
+	packages=setuptools.find_packages(),
 	include_package_data=False,
 	version='0.2.1',
 	install_requires=['click'],
