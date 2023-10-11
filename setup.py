0fea2cfff773a63718568e40ded70206506ca0ea
Karthik <kbalasubramaniam@anl.gov>
2023-10-11 17:08:36 -0400

    fix for Pip install does not work. Issue:https://github.com/openEDI/oedi-si-single-container/issues/7

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import os
 import setuptools
+import site
+
+
+data_files=[]
+relativePath='lib'+site.getsitepackages()[0].split('lib')[1]
+
+for thisFolder in ['build','docs','logs','output','runner','tmp','user_federates','user_interface']:
+	for root,dirnames,fnames in os.walk(thisFolder):
+		data_files.append((f'{os.path.join(relativePath,"oedisi_single_container",root)}',\
+		[os.path.join(os.path.abspath(root),fname) for fname in fnames]))
+
 
 setuptools.setup(name='oedisi_single_container',
 	author = 'Karthikeyan Balasubramaniam',
 	author_email='kbalasubramaniam@anl.gov',
 	packages=setuptools.find_packages(),
-	include_package_data=False,
+	data_files=data_files,
 	version='0.3.0',
 	install_requires=['click==8.1.7','pandas==2.0.3'],
 	entry_points={
