5e2c674a3c496460d3320c0002ee515985db5b27
Siby Plathottam <sibyjackgrove@gmail.com>
2024-08-16 17:24:51 -0500

    Update parse_config to use latest version of wiring diagram

--- runner/parse_config.py
+++ runner/parse_config.py
@@ -97,6 +97,7 @@ class Build(object):
 					stateEstimatorWiringData[thisFederate['name']]['components'][0]['parameters']=thisFederate['parameters']
 
 		# check federate requirement
+		print(f"Available federates:{list(availableFederates.keys())}")
 		unavailableFed=set(userConfig['federates']).difference(availableFederates)
 		assert not unavailableFed,f"federates {unavailableFed} are unavailable"
 
@@ -117,6 +118,7 @@ class Build(object):
 
 		for thisAppFederate in appFederates:
 			assert thisAppFederate in stateEstimatorWiringData,f'{thisAppFederate} not in stateEstimatorWiringData'
+			print(f"Adding {thisAppFederate} to wiring diagram...")
 			wiringDiagramData['components'].extend(stateEstimatorWiringData[thisAppFederate]['components'])
 			wiringDiagramData['links'].extend(stateEstimatorWiringData[thisAppFederate]['links'])
 
@@ -138,14 +140,16 @@ class Build(object):
 
 		# template based federate additions to wiring diagram (dopf_ornl)
 		#### TODO: Need to make changes to config data structure to include dopf federates
-		thisConf=self._get_template_federate_config('dopf_ornl','StateEstimatorComponent',\
-			'/home/run/dopf_ornl/','python /home/run/dopf_ornl/dopf_federate.py',src='/home/dopf_ornl/')
+		
+		thisConf=self._get_template_federate_config('dopf_ornl','OptimalPowerFlowORNL',
+			'/home/oedisi-dopf-ornl/','python /home/oedisi-dopf-ornl/dopf_federate/opf_federate.py',src='/home/oedisi-dopf-ornl/dopf_federate')
 		self._update_wiring_diagram_data(wiringDiagramData,thisConf)
-
+		
 		# write wiring diagram and generate config_runner
 		wiring_diagram_path=os.path.join(self._baseDir,'wiring_diagram.json')
 		json.dump(wiringDiagramData,open(wiring_diagram_path,'w'),indent=3)
-		
+		print("Wiring diagram:",wiring_diagram_path)
+		print(wiringDiagramData)
 		#Update paths in components.json
 		with open("/home/oedisi/oedisi-example/components.json", "r") as file:
 			data = json.load(file)		
@@ -153,8 +157,12 @@ class Build(object):
 		with open("/home/oedisi/oedisi-example/components.json", "w") as file: # Save the updated JSON back to the file
 			json.dump(updated_data, file, indent=4)
 		
-		#Update paths in docker_system.json
-		with open("/home/oedisi/oedisi-example/scenarios/docker_system.json", "r") as file:
+		#select system_json
+		#system_json_path = wiring_diagram_path #has download error
+		#system_json_path = '/home/runtime/runner/docker_system.json' # has measurement validation error
+		system_json_path = '/home/runtime/runner/system.json' #works
+		#Update paths in system_json
+		with open(system_json_path, "r") as file:
 			data = json.load(file)
 		for component in data["components"]:
 			if "feather_filename" in component["parameters"]:				
@@ -164,22 +172,24 @@ class Build(object):
 			if "topology_output" in component["parameters"]:				
 				component["parameters"]["topology_output"] = component["parameters"]["topology_output"].replace("../../", "/home/") #assign the result of replace back to the dictionary key.
 		
-		with open("/home/oedisi/oedisi-example/scenarios/docker_system.json", "w") as file: # Save the updated JSON back to the file
+		with open(system_json_path, "w") as file: # Save the updated JSON back to the file
 			json.dump(data, file, indent=4)
 		
 		#Create system_runner.json from components.json and docker_system.json
-		directive=f'oedisi build --target-directory /home/run --component-dict /home/oedisi/oedisi-example/components.json --system /home/oedisi/oedisi-example/scenarios/docker_system.json'
+		#directive=f'oedisi build --target-directory /home/run --component-dict /home/oedisi/oedisi-example/components.json --system /home/oedisi/oedisi-example/scenarios/docker_system.json'		
+		directive=f'oedisi build --target-directory /home/run --component-dict /home/runtime/runner/components.json --system {system_json_path}'
+		
 		print(f"Executing directive:{directive}")
 		flag=os.system(directive)
 		assert flag==0,f'generating config_runner failed with flag:{flag}'
-
+		
 		# template based federate additions (dopf_ornl)
 		if thisConf['data']['src']:# replace template data after wiring diagram puts default data
 			os.system(f'rm -r /home/run/{thisConf["configRunnerData"]["federates"]["name"]}/* && '+\
 				f'cp -r {thisConf["data"]["src"]}/* /home/run/{thisConf["configRunnerData"]["federates"]["name"]}')
-
+		
 		config_runner=json.load(open('/home/run/system_runner.json')) #Load system_runner.json into config_runner
-
+		
 		# config_runner modifications for dopf_ornl
 		ind=-1
 		for n in range(len(config_runner['federates'])):
@@ -188,7 +198,7 @@ class Build(object):
 		if ind>=0:
 			config_runner['federates'].pop(ind)
 			config_runner['federates'].append(thisConf['configRunnerData']['federates'])
-
+		
 		# broker modifications
 		if 'run_broker' in userConfig and not userConfig['run_broker']:
 			userConfig['externally_connected_federates'].append('broker')
